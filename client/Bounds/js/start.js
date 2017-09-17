b9e9f44f3f928ccd85755327245e84a5d7361d42
kna-github <nikolay.kuznetsov@gmail.com>
2017-09-17 16:19:46 +0300

    created game process

--- client/Bounds/js/start.js
+++ client/Bounds/js/start.js
@@ -9,38 +9,59 @@ var playerBet;
 var computerBet;
 var ball;
 
-var computerBetSpeed = 190;
+var computerBetSpeed = 300;
 var ballSpeed = 300;
 
 
 
 // Called after preload
 function create() {
-    game.add.tileSprite(0, 0, 480, 640, 'background');
-    game.physics.startSystem(Phaser.Physics.ARCADE)
+  game.add.tileSprite(0, 0, 480, 640, 'background');
+  game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    playerBet = createBet(game.world.centerX, 600);
-    computerBet = createBet(game.world.centerX, 20);
-    ball=createBall();
-    game.input.onDown.add(releaseBall, this);
+  playerBet = createBet(game.world.centerX, 620);
+  computerBet = createBet(game.world.centerX, 20);
+  ball=createBall();
+
+  game.input.onDown.add(releaseBall, this);
 }
 
 
 function preload() {
-    game.load.image('bet', '../assets/bet.png');
-    game.load.image('ball', '../assets/ball.png');
-    game.load.image('background', '../assets/starfield.jpg');
+  game.load.image('bet', '../assets/bet.png');
+  game.load.image('ball', '../assets/ball.png');
+  game.load.image('background', '../assets/starfield.jpg');
 }
 
 
 // Called once every frame, ideally 60 times per second
 function update() {
-  //console.log(playerBet);
+  playerBet.x = game.input.x;
+  var playerBetHalfWidth = playerBet.width / 2;
+  if(playerBet.x < playerBetHalfWidth) {
+    playerBet.x = playerBetHalfWidth;
+  } else if(playerBet.x > game.width - playerBetHalfWidth) {
+    playerBet.x = game.width - playerBetHalfWidth;
+  }
+  //рокетка компютера
+  if(computerBet.x - ball.x < -15) {
+    computerBet.body.velocity.x = computerBetSpeed;
+  } else if(computerBet.x - ball.x > 15) {
+    computerBet.body.velocity.x = -computerBetSpeed;
+  } else {
+    computerBet.body.velocity.x = 0;
+  }
+
+  //Проверяем и обрабатываем столкновения мячика и ракеток
+  game.physics.arcade.collide(ball, playerBet, ballHitsBet, null, this);
+  game.physics.arcade.collide(ball, computerBet, ballHitsBet, null, this);
+
 }
+
 
 function createBet(x, y) {
     var bet = game.add.sprite(x, y, 'bet');
-    game.physics.enable(bet, Phaser.Physics.ARCADE)
+    game.physics.arcade.enable(bet);
     bet.anchor.setTo(0.5, 0.5);
     bet.body.collideWorldBounds = true;
     bet.body.bounce.setTo(1, 1);
@@ -51,7 +72,7 @@ function createBet(x, y) {
 
 function createBall(){
     var ball = game.add.sprite(game.world.centerX, game.world.centerY, 'ball');
-    game.physics.enable(ball, Phaser.Physics.ARCADE)
+    game.physics.arcade.enable(ball);
     ball.anchor.setTo(0.5, 0.5);
     ball.body.collideWorldBounds = true;
     ball.body.bounce.setTo(1, 1);
@@ -67,4 +88,24 @@ function releaseBall() {
         ball.body.velocity.y = -ballSpeed;
         ballReleased = true;
     }
+}
+
+function ballHitsBet (_ball, _bet) {
+  /*var diff = 0;
+
+  if (_ball.x < _bet.x) {
+    //  Шарик находится с левой стороны ракетки
+    diff = _bet.x - _ball.x;
+    _ball.body.velocity.x = (-10 * diff);
+  }
+  else if (_ball.x > _bet.x) {
+    //  Шарик находится с правой стороны ракетки
+    diff = _ball.x -_bet.x;
+    _ball.body.velocity.x = (10 * diff);
+  }
+  else {
+    //  Шарик попал в центр ракетки, добавляем немножко трагической случайности его движению
+    _ball.body.velocity.x = 2 + Math.random() * 8;
+  }*/
+  
 }
